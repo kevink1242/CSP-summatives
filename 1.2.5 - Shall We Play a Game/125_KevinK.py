f9cce295b84a8be1cb35d9c4972b561407485e6f
kevink1242 <234477@shakopeeschools.org>
2025-10-27 09:43:03 -0500

    Update 125_KevinK.py
    Organizing

--- 1.2.5 - Shall We Play a Game/125_KevinK.py
+++ 1.2.5 - Shall We Play a Game/125_KevinK.py
@@ -1,21 +1,61 @@
+import turtle as trtl
+import random as rand
+
+
+#-------TURTLES--------
 # TODO 1: Initalizing turtles with onclick events that'll call on a function to randomly generate a number
 
+#----------COOKIE RELATED------------
 # TODO 2: Functions to draw out the 4 cookies
 # TODO 2.1: List storing these functions
 
+
+#-------FUNCTIONS-------
 # TODO 3: From todo 1, create a function that'll randomly generate a number
 # TODO 3.1: return a number from the function based on the cookie selected
 
 # TODO 4: List of letters 'list()' that cap out based on the number from todo 3.1
 
-# TODO 5: Display the shape of the cookie
 # TODO 6: Have letters show up next to the cookie, that will change like in apple avalanche
 
 # TODO 7: Timer
 
 # TODO 8: Point system, removing a point whenever a wrong key is pressed/adding a point whenever a right key is pressed
 
+
+
+#----------GAME-----------
+# TODO 5: Display the shape of the cookie
+
+
+
+
+#--------SCREEN--------
 # TODO 9: Win screen, showing the cookie being cracked? Displaying points and name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # TODO OPTIONAL (10): Turtle tracing the shape of the cookie as keys are being pressed (OPTIONAL)
 
