bc82155b16b7cc9bc6b8f39669d713cfe98b5f6c
kevink1242 <234477@shakopeeschools.org>
2025-11-03 08:44:24 -0600

    Update 125_KevinK.py
    changing functions to make the shapes look more accurate

--- 1.2.5 - Shall We Play a Game/125_KevinK.py
+++ 1.2.5 - Shall We Play a Game/125_KevinK.py
@@ -12,13 +12,14 @@ wn.listen()
 #------VARIABLES-----
 xcord = -265
 
-letter_list = list('QWERTYUIOPASDFGHJKLZXCVBNM')
+#letter_list = list('QWERTYUIOPASDFGHJKLZXCVBNMWERTYUIOPASDFGHJKLZXCVBQWERTYUIOPASDFGHJKLZXCVBNMWERTYUIOPASDFGHJKLZXCNMEE') # = 100 letters
+letter_list = list('QWERTYUIOPASDFGHJKLZXCVBNMWERTYUIOPASDFGHJKLZXCVBQWERTYUIOPASDFGHJKLZXCVBNMWERTYUIOPASDFGH') # = 90 letters
 currentletter = 'E'
 
 lives = 3
 
 # Timer
-totaltime = 15
+totaltime = 50
 counter_interval = 1000
 
 
@@ -52,7 +53,7 @@ penS = 5
 keysignifier = trtl.Turtle(shape='circle')
 keysignifier.hideturtle()
 keysignifier.penup()
-keysignifier.color('darkgray')
+keysignifier.color('lightgray')
 keysignifier.turtlesize(5)
 keysignifier.goto(200,0)
 
@@ -111,8 +112,9 @@ def draw_triangle():
 
     # The shape part
     painter.pencolor('black')
-    painter.goto(-200,-80)
+    painter.goto(-110,-30)
     painter.pendown()
+    painter.setheading(60)
     painter.circle(100,360,3)
     painter.penup()
 
@@ -131,11 +133,15 @@ def draw_star():
     painter.penup()
 
     # The shape part
+    painter.goto(-180,50)
     painter.pencolor('black')
-    painter.goto(-200,-80)
     painter.pendown()
-    painter.circle(100,360,6)
-    painter.penup()
+
+    for i in range(5):
+        painter.forward(75)
+        painter.right(144)
+        painter.forward(75)
+        painter.left(72)
 
 def draw_umbrella():
     painter.setheading(0)
@@ -153,10 +159,30 @@ def draw_umbrella():
 
     # The shape part
     painter.pencolor('black')
-    painter.goto(-200,-80)
+    painter.goto(-100,50)
+
+    # Top half
     painter.pendown()
+    painter.setheading(90)
     painter.circle(100,180)
+
+    painter.setheading(0)
+    for i in range(7):
+        painter.left(45)
+        painter.forward(20)
+        painter.right(90)
+        painter.forward(20)
+        painter.setheading(0)
     painter.penup()
+
+    # Handle
+    painter.goto(-200,58)
+    painter.setheading(270)
+    painter.pendown()
+    painter.forward(120)
+    painter.circle(-20,180)
+
+
 
 def draw_crack(life):
     if life >= 2:
@@ -179,7 +205,6 @@ def draw_crack(life):
             painter.forward(20)
             painter.penup()
     
-
 def draw_lose():
     painter.clear()
     painter.setheading(0)
@@ -201,28 +226,28 @@ def draw_shape(num):
     cap = 0
     if num == 1:
         draw_triangle()
-        cap = 10
+        cap = 55
     elif num == 2:
         draw_circle()
-        cap = 15
+        cap = 65
     elif num == 3:
         draw_star()
-        cap = 20
+        cap = 80
     elif num == 4:
         draw_umbrella()
-        cap = 26
+        cap = 90 
     
     list_cap(cap)
 
 def list_cap(caplength):
-    done = False
-    while done == False:
+    donecap = False
+    while donecap == False:
         if len(letter_list) != caplength:
             indexrand = rand.randint(0,len(letter_list)-1)
             letter_list.pop(indexrand)
 
         else:
-            done = True
+            donecap = True
 
 def check_key(key):
     # Dont need 'global currentletter' as the variable isn't being changed
@@ -286,7 +311,7 @@ def game_end(win, timerup):
         draw_lose()
         
     elif win == False and timerup == False: # Ran out of lives
-        print('cracked the cookie')
+        timekeeper.clear()
         draw_lose()
 
 # TODO 9: make the lines on the cookie line up matching up with the amount of keys pressed
@@ -321,6 +346,7 @@ for d in placeholder_list:
 draw_shape(userselection)
 draw_letter(currentletter)
 timer()
+
 
 for letter in 'qwertyuiopasdfghjklzxcvbnm':
     wn.onkeypress(lambda l=letter: check_key(l), letter)
