a373d10205f2f616a6157d13ad8ac0e75da743f8
kevink1242 <234477@shakopeeschools.org>
2025-11-11 09:20:34 -0600

    Create Artistic_Expression-M&K.py
    todos to make the greeting card function

---
+++ 1.3.1 - Artistic Expression/Artistic_Expression-M&K.py
@@ -0,0 +1,15 @@
+# TODO 1: Import the envelope images
+
+# TODO 2: Change the image of the envelope to an opened picture when clicked on
+
+# TODO 3: Store all the puzzle pieces in a list
+
+# TODO 4: Have the puzzle pieces come out of the envelope and go to different places (function)
+
+# TODO 5: Make each puzzle piece able to be dragged
+
+# TODO 6: Conditional statement if the puzzle piece comes close enough to a (x,y) coordinate and disabling the drag function
+
+# TODO 7: Option to restart the puzzle?
+
+# TODO 8: Make everything clearly able to be read/seen by an elderly person
